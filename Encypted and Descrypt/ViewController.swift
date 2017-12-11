517ab4e74c4a017a73d9ac92456f32eed6adef50
oliverpople <oliverpople@gmail.com>
2017-12-11 20:03:31 +0000

    Got encryption app working

--- Encypted and Descrypt/ViewController.swift
+++ Encypted and Descrypt/ViewController.swift
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
         let input = stringToEn.text
         let key = keyEn.text
         let iv = "gqLOHUioQ0QjhuvI"
-        let en = try! input!.aesEncrypt(key: key!, iv: iv)
+        let en = try! input!.aesEncrypt(key!, iv: iv)
         outputEn.text = en
         
         
@@ -45,21 +45,21 @@ class ViewController: UIViewController {
         let input = stringToDes.text
         let key = keyDes.text
         let iv = "gqLOHUioQ0QjhuvI"
-        let des = try! input!.aesDecrypt(key: key!, iv: iv)
+        let des = try! input!.aesDecrypt(key!, iv: iv)
         outputDes.text = des
         
     }
 
 }
 extension String{
-    func aesEncrypt(key: String, iv: String) throws -> String {
+    func aesEncrypt(_ key: String, iv: String) throws -> String {
         let data = self.data(using: .utf8)!
         let encrypted = try! AES(key: key, iv: iv, blockMode: .CBC, padding: PKCS7()).encrypt([UInt8](data))
         let encryptedData = Data(encrypted)
         return encryptedData.base64EncodedString()
     }
     
-    func aesDecrypt(key: String, iv: String) throws -> String {
+    func aesDecrypt(_ key: String, iv: String) throws -> String {
         let data = Data(base64Encoded: self)!
         let decrypted = try! AES(key: key, iv: iv, blockMode: .CBC, padding: PKCS7()).decrypt([UInt8](data))
         let decryptedData = Data(decrypted)
